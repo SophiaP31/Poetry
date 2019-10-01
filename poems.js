9fa311a8b9c1a897f72b64ff380df7059f1a6cc2
sophiap31 <sap8ht@virginia.edu>
2019-09-30 21:34:50 -0400

    changed font, added poem

---
+++ poems.js
@@ -0,0 +1,3 @@
+{
+poem1: "Estira\nlengua de pájaro\nque está perdido\nmi disfraz\nen una caravana con garras\nque inunda\nhabitaciones\ny el sótano de sueños\npara saltar\ncon mis panteras y mis pedazos\nconmigo en la jaula\nabierta\nsin domador",
+}
